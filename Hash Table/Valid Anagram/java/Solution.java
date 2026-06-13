4a63217ccd008a7a0936b3e31674260c1b85d579
Deepak P <deepak.vsbec@gmail.com>
2026-06-13 14:49:37 +0530

    feat: Add Valid Anagram

--- Hash Table/Valid Anagram/java/Solution.java
+++ Hash Table/Valid Anagram/java/Solution.java
@@ -1,19 +1,11 @@
 import java.util.Arrays;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-
         if(s.length() != t.length()) return false; //edge case 
-
-        // convert string  in character array 
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
-        
-         //sort character lexicographically
         Arrays.sort(sChars);  
         Arrays.sort(tChars);
-        
-        // check if both sorted arrays are identical
         return Arrays.equals(sChars, tChars);
     }
 }
