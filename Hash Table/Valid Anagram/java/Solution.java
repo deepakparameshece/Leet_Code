1b328772c8d54af1bd7b8bfcaf3bd853e9c8c65c
Deepak P <deepak.vsbec@gmail.com>
2026-06-13 14:40:26 +0530

    feat: Add Valid Anagram

--- Hash Table/Valid Anagram/java/Solution.java
+++ Hash Table/Valid Anagram/java/Solution.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) 
-        return false;
-        char[]sarr = s.toCharArray();
-        char[]tarr=t.toCharArray();
-        Arrays.sort(sarr);
-        Arrays.sort(tarr);
-        return Arrays.equals(sarr,tarr);
-    }}
+        if (s.length() != t.length()) return false;
         
+        int xorSum = 0;
+        int[] frequency = new int[26];
         
+        for (int i = 0; i < s.length(); i++) {
+            xorSum ^= s.charAt(i) ^ t.charAt(i); // XOR both characters
+            frequency[s.charAt(i) - 'a']++; // Increment for s
+            frequency[t.charAt(i) - 'a']--; // Decrement for t
+        }
+        
+        if (xorSum != 0) return false; // If XOR isn't zero, they're not anagrams
+        
+        for (int freq : frequency) {
+            if (freq != 0) return false; // Check frequency counts
+        }
+        
+        return true;
+    }
+}
