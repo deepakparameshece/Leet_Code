6256f8d421f61fdc6e7631efd261952f778cb0ea
Deepak P <deepak.vsbec@gmail.com>
2026-06-13 14:48:09 +0530

    feat: Add Valid Anagram

--- Hash Table/Valid Anagram/java/Solution.java
+++ Hash Table/Valid Anagram/java/Solution.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-    if(s.length() != t.length()) return false;
-    char[] sChars = s.toCharArray();
-    char[] tChars = t.toCharArray();
-    System.out.println(Arrays.equals(sChars,tChars));
-    }}
+
+        if(s.length() != t.length()) return false; //edge case 
+
+        // convert string  in character array 
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+         //sort character lexicographically
+        Arrays.sort(sChars);  
+        Arrays.sort(tChars);
+        
+        // check if both sorted arrays are identical
+        return Arrays.equals(sChars, tChars);
+    }
 }
