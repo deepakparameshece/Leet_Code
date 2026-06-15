4656bf1012f5b7d5f653ac96f4ada0b3cf67cdfd
Deepak P <deepak.vsbec@gmail.com>
2026-06-15 16:37:37 +0530

    feat: Add Add Digits

--- Math/Add Digits/java/Solution.java
+++ Math/Add Digits/java/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int addDigits(int num) {
-        while (num >= 10) {       
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;  
-                num /= 10;        
+        while(num>9){
+            int sum=0;
+            while(num>0){
+                sum+=num%10;
+                num/=10;
             }
-            num = sum;            
+            num=sum;
         }
         return num;
     }
