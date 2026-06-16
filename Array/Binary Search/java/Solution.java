f02a8af70bd8b73f0027bc0076f7156caf4484c2
Deepak P <deepak.vsbec@gmail.com>
2026-06-16 14:27:28 +0530

    feat: Add Binary Search

--- Array/Binary Search/java/Solution.java
+++ Array/Binary Search/java/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
       int left=0;
-      int right =nums.length;
+      int right =nums.length-1;
       while(left<=right){
         int m=left+(right-left)/2;
         if(nums[m]==target) return m;
