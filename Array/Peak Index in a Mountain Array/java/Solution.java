3e69c14c20d31ac0a1968aa5d6cf58370e09dccc
Deepak P <deepak.vsbec@gmail.com>
2026-06-16 15:22:38 +0530

    feat: Add Peak Index in a Mountain Array

--- Array/Peak Index in a Mountain Array/java/Solution.java
+++ Array/Peak Index in a Mountain Array/java/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
     int left=0;
       int right =arr.length-1;
-      while(left<=right){
+      while(left<right){
         int m=left+(right-left)/2;
         if(arr[m]<arr[m+1]) left=m+1;
         else right=m;
