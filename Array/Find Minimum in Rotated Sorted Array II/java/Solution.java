7f764688db34ecb86929674e2f8ecccd0da03c05
Deepak P <deepak.vsbec@gmail.com>
2026-06-16 16:18:53 +0530

    feat: Add Find Minimum in Rotated Sorted Array II

---
+++ Array/Find Minimum in Rotated Sorted Array II/java/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int left=0;
+      int right =arr.length-1;
+      while(left<right){
+        int m=left+(right-left)/2;
+        if(arr[m]>arr[right]) left=m+1;
+        else right=m;
+      }
+      return arr[left];
+
+    }
+}
