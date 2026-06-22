0ca5d97f8eea31eb088357239eb11d6490e05573
Deepak P <deepak.vsbec@gmail.com>
2026-06-22 15:09:23 +0530

    feat: Add Intersection of Two Arrays

--- Array/Intersection of Two Arrays/java/Solution.java
+++ Array/Intersection of Two Arrays/java/Solution.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-         
-         ArrayList<Integer> list = new ArrayList<>() ;
-
-        // convert one array into hashset , therefore it will have only unique elements
-        HashSet<Integer> set = new HashSet<>() ;
-        for( int i = 0 ; i < nums1.length ; i++){
-            set.add(nums1[i]) ;
+        Set<Integer> set1 = new HashSet<>();
+        for (int i : nums1) {
+            set1.add(i);
         }
-
-        //ab ek array me unique element hai , to ham har unique element ko check karenge
-        //dusre set se ki usme hai ya nhi.
-
-        for(int i = 0 ; i < nums2.length ; i++){
-            if( set.contains(nums2[i])){
-                list.add(nums2[i]) ;
-                set.remove(nums2[i]) ; // jo ek baar agaya use set se delete krdo taki repeat na ho
-            }
+        Set<Integer> set2 = new HashSet<>();
+        for (int i : nums2) {
+            set2.add(i);
         }
-
-        //convert the list into array
-        int[] ans = new int[list.size()] ; 
-        for(int i= 0 ; i < list.size() ; i++){
-            ans[i] = list.get(i) ;
+        set1.retainAll(set2);
+        int[] arr = new int[set1.size()];
+        int i=0;
+        for(int n:set1){
+            arr[i++]=n;
         }
+                return arr;
 
-        return ans; 
     }
 }
