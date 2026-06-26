b5da38db6396dad608b5b4ce76cc0559eb30c3a0
Deepak P <deepak.vsbec@gmail.com>
2026-06-26 14:28:30 +0530

    feat: Add Best Time to Buy and Sell Stock

--- Array/Best Time to Buy and Sell Stock/java/Solution.java
+++ Array/Best Time to Buy and Sell Stock/java/Solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int profit = Integer.MIN_VALUE;
-        int var = prices[n - 1];
-        // create a max array or rather find element of max array at that index and calculate profit 
-        for (int i = n - 1; i >= 0; i--) {
-            int maxElement = var;
-            profit = Math.max(profit, maxElement - prices[i]);
-            if (prices[i] > var) var = prices[i];
+      int minPrice=prices[0];
+      int profit=0;
+      for(int i=1;i<prices.length;i++){
+        if(prices[i]<minPrice){
+            minPrice=prices[i];
         }
-        return profit;
+        profit=Math.max(profit,prices[i]-minPrice);
+      } 
+      return profit;
     }
 }
