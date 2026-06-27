01582a236e98287971a5299249bc412cbb6b3c78
Deepak P <deepak.vsbec@gmail.com>
2026-06-27 15:29:59 +0530

    feat: Add Merge Intervals

--- Array/Merge Intervals/java/Solution.java
+++ Array/Merge Intervals/java/Solution.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
         List<int[]> list=new ArrayList<>();
         int [] current=intervals[0];
-        for(int i=1;i<interval.length;i++){
-            if(interval[i][0]<=current[1]){
-                current[1]=Math.max(current[1],interval[i][1]);
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=current[1]){
+                current[1]=Math.max(current[1],intervals[i][1]);
             }
             else{
                 list.add(current);
